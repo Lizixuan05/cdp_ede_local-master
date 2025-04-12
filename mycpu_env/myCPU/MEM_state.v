55e9c091a2c228f8051e4c4c41536be50a9d2a31
lizixuan <13001543682@163.com>
2025-04-12 17:54:47 +0800

    MEM_state

---
+++ mycpu_env/myCPU/MEM_state.v
@@ -0,0 +1,77 @@
+module MEM_state (
+    input  wire        clk,
+    input  wire        reset,
+
+    //EXE_MEM interface
+    /*state control*/
+    input  wire        EXE_MEM_valid,
+    output wire        MEM_allowin,
+    /*buffer*/
+    input  wire [31:0] EXE_pc,
+    input  wire [31:0] EXE_alu_result,
+    input  wire [ 5:0] EXE_rf,//{rf_we[5],rf_waddr[4:0]}
+    input  wire [33:0] EXE_mem,//{mem_we[33],res_from_mem[32],rkd_value[31:0]}
+
+    //MEM_WB interface
+    /*state control*/
+    output wire        MEM_WB_valid,
+    input  wire        WB_allowin,
+    /*buffer*/
+    output reg  [31:0] MEM_pc,
+    output wire [37:0] MEM_rf,//{rf_wdata[37:6],rf_we[5],rf_waddr[4:0]}
+
+    // data sram interface
+    output wire        data_sram_en,
+    output wire [ 3:0] data_sram_we,
+    output wire [31:0] data_sram_addr,
+    output wire [31:0] data_sram_wdata,
+    input  wire [31:0] data_sram_rdata
+);
+    wire MEM_ready_go;
+    reg  MEM_valid;
+    reg [31:0] mem_alu_result;
+    reg [5:0] mem_rf;
+    wire [31:0] mem_rf_wdata;
+    reg [31:0] mem_rkd_value;
+    reg mem_res_from_mem;
+    reg mem_mem_we;
+    wire [31:0] mem_result;
+
+    /*------------------EXE_MEM buffer----------------------*/
+
+    always @(posedge clk) begin
+        if (MEM_allowin & EXE_MEM_valid) begin
+            MEM_pc <= EXE_pc;
+            mem_alu_result <= EXE_alu_result;
+            mem_rf <= EXE_rf;
+            {mem_mem_we,mem_res_from_mem,mem_rkd_value} <= EXE_mem;
+        end
+    end
+
+    /*------------------state control------------------------*/
+    assign MEM_ready_go = 1'b1;
+    assign MEM_allowin = ~MEM_valid | MEM_ready_go & WB_allowin;
+    assign MEM_WB_valid = MEM_valid & MEM_ready_go;
+    always @(posedge clk) begin
+        if (reset) begin
+            MEM_valid <= 1'b0;
+        end
+        else begin
+            MEM_valid <= MEM_allowin & EXE_MEM_valid;
+        end
+    end
+
+    /*---------------data sram control---------------------*/
+    //注意时序问题
+    assign data_sram_en = EXE_mem[33] | EXE_mem[32];
+    assign data_sram_we = {4{EXE_mem[33]}};
+    assign data_sram_addr = EXE_alu_result;
+    assign data_sram_wdata = EXE_mem[31:0];
+
+    /*--------------MEM_WB buffer--------------------------*/
+    assign mem_result = data_sram_rdata;
+    assign mem_rf_wdata = mem_res_from_mem?mem_result:mem_alu_result;
+    assign MEM_rf = {mem_rf_wdata, mem_rf[5:0]};
+
+
+endmodule
--- mycpu_env/myCPU/mycpu_top.v
+++ mycpu_env/myCPU/mycpu_top.v
@@ -9,7 +9,7 @@ module mycpu_top(
     input  wire [31:0] inst_sram_rdata,
     // data sram interface
     output wire        data_sram_en,
-    output wire        data_sram_we,
+    output wire [ 3:0] data_sram_we,
     output wire [31:0] data_sram_addr,
     output wire [31:0] data_sram_wdata,
     input  wire [31:0] data_sram_rdata,
@@ -22,39 +22,6 @@ module mycpu_top(
 
 reg         reset;
 always @(posedge clk) reset <= ~resetn;
-
-wire        load_op;//?
-
-wire [31:0] alu_result ;
-
-wire [31:0] mem_result;
-wire [31:0] final_result;
-
-
-
-alu u_alu(
-    .alu_op     (alu_op    ),
-    .alu_src1   (alu_src1  ),
-    .alu_src2   (alu_src2  ),
-    .alu_result (alu_result)
-    );
-
-assign data_sram_we    = mem_we && valid;
-assign data_sram_addr  = alu_result;
-assign data_sram_wdata = inst_st_w?rkd_value:
-                         inst_st_b?{data_sram_rdata[31:8], rkd_value[ 7:0]}:
-                         inst_st_h?{data_sram_rdata[31:16], rkd_value[15:0]}:
-                         32'b0;
-
-assign mem_result   =  inst_ld_w ? data_sram_rdata :
-                       inst_ld_b ? {{24{data_sram_rdata[ 7]}}, data_sram_rdata[ 7:0]} :
-                       inst_ld_h ? {{16{data_sram_rdata[15]}}, data_sram_rdata[15:0]} :
-                       inst_ld_bu? {24'b0, data_sram_rdata[ 7:0]} :
-                       inst_ld_hu? {16'b0, data_sram_rdata[15:0]} : 32'b0;
-
-assign final_result = res_from_mem ? mem_result : alu_result;
-
-
 
 // debug info generate
 assign debug_wb_pc       = pc;
